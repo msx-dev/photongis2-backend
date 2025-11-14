b8c05a9006c32a0feec834c6b973fd33db137df4
msx-dev <maxim.sramek@gmail.com>
2025-11-14 13:07:26 +0100

    add localhost for dev purposes

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 from contextlib import asynccontextmanager
 from utils import create_tables
 from routers import auth_router, user_router, projects_router, rooftops_router
+from fastapi.middleware.cors import CORSMiddleware
 
 
 @asynccontextmanager
@@ -16,6 +17,18 @@ app.include_router(router=auth_router)
 app.include_router(user_router)
 app.include_router(projects_router)
 app.include_router(rooftops_router)
+
+origins = [
+    "http://localhost:3000",  # DEV
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.get("/health")
